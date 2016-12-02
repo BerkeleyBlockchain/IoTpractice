9f0cabd8003c5985d84a152faf193680a2c0d51c
graham-hughes <grahamhughes@airbears2-10-142-174-132.airbears2.1918.berkeley.edu>
2016-12-01 19:58:15 -0800

    Finished first draft of sensor consensus contract

--- contract.js
+++ contract.js
@@ -3,12 +3,10 @@ contract rpsAdvanced
     mapping (string => mapping(string => int)) payoffMatrix;
     address [] sensors;
     int [] choices;
-    uint firstRevealTime;
-
 
     modifier notRegisteredYet()
     {
-        if (msg.sender in sensors)
+        if (inArray(msg.sender, sensors))
             throw;
         else
             _
@@ -16,7 +14,7 @@ contract rpsAdvanced
 
     modifier isRegistered()
     {
-        if (msg.sender not in sensors)
+        if (! inArray(msg.sender, sensors))
             throw;
         else
             _
@@ -24,35 +22,28 @@ contract rpsAdvanced
 
     modifier sentEnoughCash(uint amount)
     {
-        if (msg.value != amount)
+        if (msg.value < amount) 
             throw;
         else
             _
     }
     
-    modifier validChoice(string choice)
+    modifier validChoice(uint choice)
     {
         // hack until we can use StringUtils.equal
-        if (typeof(choice) != number) //Checks if the choice is a number
+        // makes sure the choice is 0 or 1 (0 - False, 1 - True)
+        if (choice != 0 && choice != 1) 
             throw;
         else
             _
     }
     
     function rpsAdvanced() 
-    {   // constructor (spoiler alert: rename this if you rename the contract!)
-        // Edit this to determine 
-        payoffMatrix["rock"]["rock"] = 0;
-        payoffMatrix["rock"]["paper"] = 2;
-        payoffMatrix["rock"]["scissors"] = 1;
-        payoffMatrix["paper"]["rock"] = 1;
-        payoffMatrix["paper"]["paper"] = 0;
-        payoffMatrix["paper"]["scissors"] = 2;
-        payoffMatrix["scissors"]["rock"] = 2;
-        payoffMatrix["scissors"]["paper"] = 1;
-        payoffMatrix["scissors"]["scissors"] = 0;
+    {   // constructor (rename this if you rename the contract)
     }
-    
+
+     
+
     function register()
         sentEnoughCash(5)
         notRegisteredYet
@@ -64,62 +55,44 @@ contract rpsAdvanced
         isRegistered
         validChoice(choice)
     {
-
-        if (msg.sender == player1)
-            player1ChoiceHash = sha3(sha3(choice) ^ sha3(randStr));
-        else if (msg.sender == player2)
-            player2ChoiceHash = sha3(sha3(choice) ^ sha3(randStr));
+        choices.push(choice)
     }
-    
-    function reveal(string choice, string randStr)
-        isRegistered
-        validChoice(choice)
-    {
-        // second player has 120 seconds after first player revealed
-        if (bytes(player1Choice).length == 0 && bytes(player2Choice).length == 0)
-            firstRevealTime == now;
 
-        // if hashed choice + randStr is matching the initial one, choice is stored
-        if (msg.sender == player1 && sha3(sha3(choice) ^ sha3(randStr)) == player1ChoiceHash)
-            player1Choice = choice;
-        if (msg.sender == player2 && sha3(sha3(choice) ^ sha3(randStr)) == player2ChoiceHash)
-            player2Choice = choice;
-    }
     
     function checkWinner()
     {
-        if (bytes(player1Choice).length != 0 && bytes(player2Choice).length != 0)
+        var consensus = 0 
+        var sum = 0
+        var count=choices.length;
+        for(var i=0;i<count;i++)
         {
-            // if both revealed, obtain winner in usual way
-            int winner = payoffMatrix[player1Choice][player2Choice];
-            if (winner == 1)
-                player1.send(this.balance);
-            else if (winner == 2)
-                player2.send(this.balance);
-            else
-            {
-                player1.send(this.balance/2);
-                player2.send(this.balance);
-            }
+            sum = sum + choices[i]
+        }
 
-            // unregister players and choices
-            player1Choice = "";
-            player2Choice = "";
-            player1 = 0;
-            player2 = 0;
-        }
-        else if (now > firstRevealTime + 120)
+        if(sum > choices.length/2) //simple majority determines consensus
+            consensus = 1;
+        else
+            consensus = 1;
+
+        for(var i=0;i<sensors.length;i++)
         {
-            // if only one player revealed and time > start + timeout, winner is the one who revealed first
-            if (bytes(player1Choice).length != 0)
-                player1.send(this.balance);
-            else if (bytes(player2Choice).length != 0)
-                player2.send(this.balance);
+            if(sensors[i] == consensus)
+                sensors[i].send(this.balance/sum) //sends winners their share of the pot
         }
-        
     }
     
     // HELPER FUNCTIONS
+    function inArray(needle,haystack)
+    {
+        var count=haystack.length;
+        for(var i=0;i<count;i++)
+        {
+            if(haystack[i]===needle){return true;}
+        }
+        return false;
+    }  
+
+
     function getMyBalance () constant returns (uint amount)
     {
         return msg.sender.balance;
