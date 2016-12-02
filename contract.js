7464d3476cbf1415424f6c983628aeb93c44fd79
graham-hughes <grahamhughes@airbears2-10-142-174-132.airbears2.1918.berkeley.edu>
2016-12-01 20:56:25 -0800

    Added oraclize

--- contract.js
+++ contract.js
@@ -1,107 +1,16 @@
-contract rpsAdvanced
-{
-    mapping (string => mapping(string => int)) payoffMatrix;
-    address [] sensors;
-    int [] choices;
+import "dev.oraclize.it/api.sol";
 
-    modifier notRegisteredYet()
-    {
-        if (inArray(msg.sender, sensors))
-            throw;
-        else
-            _
+contract KrakenPriceTicker is usingOraclize {
+    string public myIP;
+
+    function SetUrl() {
+        oraclize_setNetwork(networkID_testnet);
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        oraclize_query("URL", "json(http://ip.jsontest.com/ ");
     }
 
-    modifier isRegistered()
-    {
-        if (! inArray(msg.sender, sensors))
-            throw;
-        else
-            _
+    function __callback(bytes32 myid, string result, bytes proof) {
+        if (msg.sender != oraclize_cbAddress()) throw;
+        myIP = result;
     }
-
-    modifier sentEnoughCash(uint amount)
-    {
-        if (msg.value < amount) 
-            throw;
-        else
-            _
-    }
-    
-    modifier validChoice(uint choice)
-    {
-        // hack until we can use StringUtils.equal
-        // makes sure the choice is 0 or 1 (0 - False, 1 - True)
-        if (choice != 0 && choice != 1) 
-            throw;
-        else
-            _
-    }
-    
-    function rpsAdvanced() 
-    {   // constructor (rename this if you rename the contract)
-    }
-
-     
-
-    function register()
-        sentEnoughCash(5)
-        notRegisteredYet
-    {
-        sensors.push(msg.sender)
-    }
-
-    function play(int choice) 
-        isRegistered
-        validChoice(choice)
-    {
-        choices.push(choice)
-    }
-
-    
-    function checkWinner()
-    {
-        var consensus = 0 
-        var sum = 0
-        var count=choices.length;
-        for(var i=0;i<count;i++)
-        {
-            sum = sum + choices[i]
-        }
-
-        if(sum > choices.length/2) //simple majority determines consensus
-            consensus = 1;
-        else
-            consensus = 1;
-
-        for(var i=0;i<sensors.length;i++)
-        {
-            if(sensors[i] == consensus)
-                sensors[i].send(this.balance/sum) //sends winners their share of the pot
-        }
-    }
-    
-    // HELPER FUNCTIONS
-    function inArray(needle,haystack)
-    {
-        var count=haystack.length;
-        for(var i=0;i<count;i++)
-        {
-            if(haystack[i]===needle){return true;}
-        }
-        return false;
-    }  
-
-
-    function getMyBalance () constant returns (uint amount)
-    {
-        return msg.sender.balance;
-    }
-    
-    function getContractBalance () constant returns (uint amount)
-    {
-        return this.balance;
-    }
-    
-    // \HELPER FUNCTIONS
-}
+} 
