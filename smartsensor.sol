bfa124f4a9302a1c57cdbf34fa0380ddf6963488
aparna krishnan <aparnalocked@gmail.com>
2016-12-03 15:33:30 -0800

    smartsensor contract

---
+++ smartsensor.sol
@@ -0,0 +1,21 @@
+contract smartsensor{
+	uint sensor1val = 0;
+	uint sensor2val = 0;
+	uint sensor3val = 0;
+	function smartsensor(uint sensor1,uint sensor2, uint sensor3){
+		sensor1val = sensor1;
+		sensor2val = sensor2;
+		sensor3val = sensor3;
+	}
+	function isLightsOff() constant returns (bool){
+		if(sensor1 > 0 && sensor2 > 0){
+			return true;
+		}else if(sensor3 > 0 && sensor2 > 0){
+			return true;
+		}else if(sensor1 > 0 && sensor2 > 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
